8c476ed3e4ee765d11dbc94a3139718bd94743ed
_mjansen <50603896+MayrlaJansen@users.noreply.github.com>
2019-09-18 12:06:36 -0300

    Add files via upload

---
+++ AtvPratica1_ED2-MayrlaJansen/src/algoritmosDeOrdenacao.py
@@ -0,0 +1,120 @@
+'''
+Introdução:
+- Implementar algoritmo de ordenação que receba uma colecão
+- A coleção é uma lista de arestas
+- Para comparar o peso as arestas entre dois item da coleção basta usar a chave 'weight' (peso)
+
+Exemplos:
+- Modo convencional
+colecao[i] operador de comparacao colecao[j]
+colecao[i] < colecao[j]
+
+- Modo que você vai usar
+int(colecao[i]['weight']) operador de comparacao int(colecao[j]['weight'])
+int(colecao[i]['weight']) < int(colecao[j]['weight'])
+
+É nescessário converter o valor pra Interger no momento da comparação a fim de evitar erros
+'''
+import random
+
+# Sua classe algoritmo de ordenação precisar ter um método ordenar
+class InsertionSort(object):
+    def ordenar(self, colecao):
+        '''
+        O método ordenar recebe uma colecão
+        realiza ordenacão na colecão
+        retorna colecão após ordenação
+        '''
+        for i in range(1, len(colecao)): #percorre da 2 posicao ate o final
+            key = colecao[i] #representa o indice do laco a ser ordenado
+            j = i-1 #usa-se para "excluir" a parte ja ordenada
+            while j >=0 and int(key['weight']) < int(colecao[j]['weight']) : #faz as comparacoes da chave com a parte que ainda não foi ordenada
+                colecao[j+1] = colecao[j] #caso seja menor, move para uma posicao a frente da posicao atual
+                j -= 1 #decremento de j
+            colecao[j+1] = key #realiza-se a troca final
+        return colecao
+
+
+class MergeSort(object):
+    def ordenar(self, colecao):
+
+        if len(colecao)>1: #testa se o tamanho da colecao e maior que 1
+            mid = int(len(colecao)//2) #acha o meio da colecao
+            lefthalf = colecao[:mid] #divide para o lado esquerdo
+            righthalf = colecao[mid:] #divide para o lado direito
+
+            #recursion
+            ##caso o teste dê errado, alterar para "self.mergeSort(lefthalf)"
+            self.ordenar(lefthalf) #chamada recursiva para o lado esquerdo
+            self.ordenar(righthalf) #chamada recursiva para o lado direito
+
+
+            i=0
+            j=0
+            k=0
+
+            while i < len(lefthalf) and j < len(righthalf): #laco que vai do primeiro elemento ate o final das listas criadas
+                if int(lefthalf[i]['weight']) < int(righthalf[j]['weight']): #teste de comparacao
+                    colecao[k]=lefthalf[i] #caso o elemento da esquerda seja menor, coloca-o na posicao 0 da colecao
+                    i=i+1 #incremento de i
+                else:
+                    colecao[k]=righthalf[j] #caso o elemento da direita seja menor, coloca-o na posicao 0 da colecao
+                    j=j+1 #incremento de j
+                k=k+1 #incremento de k
+
+            while i < len(lefthalf): #atribuicao da lista ao final da colecao, caso o da direita termine e ainda tenha elementos na esquerda
+                colecao[k]=lefthalf[i] 
+                i=i+1
+                k=k+1
+
+            while j < len(righthalf):#atribuicao da lista ao final da colecao, caso o da esquerda termine e ainda tenha elementos na direita
+                colecao[k]=righthalf[j]
+                j=j+1
+                k=k+1
+
+
+        return colecao
+
+   
+
+
+
+
+class QuickSort(object):
+    def ordenar(self,colecao):
+        self.sort(colecao, 0, len(colecao)-1)
+
+        return colecao
+
+    def particao(self, colecao, ini, fim):
+        pivo = colecao[fim-1]
+        start = ini
+        end = ini
+        for i in range(ini, fim):
+            if int(colecao[i]['weight']) > int(pivo['weight']):
+                end += 1
+            else:
+                end += 1       
+                start += 1
+                colecao[i], colecao[start-1] = colecao[start-1], colecao[i]
+        return start-1
+        
+    def sort(self, colecao, ini, fim):
+        if ini < fim:
+            pp = self.randparticao(colecao, ini, fim)
+            self.sort(colecao, ini, pp)
+            self.sort(colecao, pp+1,fim)
+    
+    def randparticao(self,colecao,ini,fim):
+        rand = random.randrange(ini,fim)
+        colecao[rand], colecao[fim-1] = colecao[fim-1], colecao[rand]
+        return self.particao(colecao,ini,fim)
+            
+
+
+
+
+
+
+        
+ 
---
+++ AtvPratica1_ED2-MayrlaJansen/src/main.py
@@ -0,0 +1,82 @@
+from grafo import Grafo
+from algoritmosDeOrdenacao import *
+from utils import *
+import random
+import datetime
+'''
+Implemente o algoritmo de ordenação no arquivo algoritmosDeOrdenacao.py
+Instruções básicas de como fazer a implementação estão no arquivo algoritmosDeOrdenacao.py
+'''
+
+if __name__ == "__main__":
+    print("Informe qual Algoritmo de Ordenaçao deseja usar:")
+
+    escolha = input("1 - QuickSort\n2 - MergeSort\n3 - InsertionSort\n4 - Quicksort + Inserção Parcial\n"
+                "5 - Quicksort + Inserção Final\n6 - Mergesort + Inserção Parcial\n7 - Mergesort + Inserção Final\n")
+    if int(escolha) == 1:
+        algoritimoDeOrdenacao = QuickSort()
+    elif int(escolha) == 2:
+        algoritimoDeOrdenacao = MergeSort()
+    elif int(escolha) == 3:
+        algoritimoDeOrdenacao = InsertionSort()
+    # elif int(escolha) == 4:
+    #     L = input("Informe o tamanho desejado para o L:")
+    #     print(L)
+    #     algoritimoDeOrdenacao = QuicksortInserçãoParcial()
+    # elif int(escolha) == 5:
+    #     L = input("Informe o tamanho desejado para o L:")
+    #     algoritimoDeOrdenacao =  QuicksortInserçãoFinal()
+    # elif int(escolha) == 6:
+    #     L = input("Informe o tamanho desejado para o L:")
+    #     algoritimoDeOrdenacao = MergesortInserçãoParcial()
+    # elif int(escolha) == 7:
+    #     L = input("Informe o tamanho desejado para o L:")
+    #     algoritimoDeOrdenacao = MergesortInserçãoFinal()
+
+
+
+    print("Informe qual arquivo de Vertices deseja usar:")
+    vertices = input("1 - 7 vertices\n2 - 100 vertices\n3 - 1000 vertices\n4 - 10000 vertices\n5 - 100000 vertices\n")
+    if int(vertices) == 1:
+        arquivoJson = '../grafos/7vertices.json'
+
+    elif int(vertices) == 2:
+        arquivoJson = '../grafos/100vertices.json' 
+
+    elif int(vertices) == 3:
+        arquivoJson = '../grafos/1000vertices.json'
+
+    elif int(vertices) == 4:
+        arquivoJson = '../grafos/10000vertices.json'
+      
+    elif int(vertices) == 5:
+        arquivoJson = '../grafos/100000vertices.json'
+
+    else:
+        print("Nenhum tamanho de entrada apresentado foi selecionado")
+        
+    
+    
+    
+    arq = str(random.randint(1,100000))
+    arquivoDeSaida = '../arvores_geradas/tree'+arq+'Vertices.txt' 
+    
+    # algoritimoDeOrdenacao = InsertionSort()
+    # arquivoJson = '../grafos/7vertices.json'
+    
+    inicio = datetime.datetime.now()
+
+    grafo = Grafo()
+    grafo.estabelecerAlgoritmoDeOrdencao(algoritimoDeOrdenacao)
+    grafo.carregarGrafo(arquivoJson)
+
+    arvoreGeradoraMinima =  grafo.executarKruskal() 
+    SalvarArvoreGeradoraMinimaEmArquivo(arquivoDeSaida, arvoreGeradoraMinima)
+
+    print ("nome do arquivo gerado:",arquivoDeSaida)
+    fim = datetime.datetime.now()
+
+    tempo = fim- inicio
+
+    print ("tempo total de execucao do algoritmo:", tempo) 
+
